16a652859751fe02777ba331f3c8fd39ec007f11
Farhan Shaikh <programmingwithfarhan@gmail.com>
2024-07-29 19:00:24 +0530

    set client

--- src/app/(home)/_layout.tsx
+++ src/app/(home)/_layout.tsx
@@ -1,5 +1,22 @@
 import { Slot, Stack } from "expo-router";
+import { useEffect } from "react";
+import { StreamChat } from 'stream-chat';
+
+const client = StreamChat.getInstance('wfxy4f9quwxh');
 
 export default function HomeLayout() {
+    useEffect(() => {
+      const connect = async () => {
+        await client.connectUser(
+            {
+              id: 'jlahey',
+              name: 'Jim Lahey',
+              image: 'https://i.imgur.com/fR9Jz14.png',
+            },
+            'user_token',
+          );
+      }
+    }, [])
+    
     return <Slot />
 }
