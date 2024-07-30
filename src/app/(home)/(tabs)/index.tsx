0a9299303f18f94aa7821816f0c01486c6ab1deb
Farhan Shaikh <programmingwithfarhan@gmail.com>
2024-07-30 18:14:54 +0530

    stream chat

--- src/app/(home)/(tabs)/index.tsx
+++ src/app/(home)/(tabs)/index.tsx
@@ -1,5 +1,9 @@
-import { Text } from "react-native";
+import { router } from "expo-router";
+import { useState } from "react";
+import { ChannelList} from "stream-chat-expo";
 
 export default function MainTabScreen() {
-    return <Text>Main tab farhan</Text>
+    const [channel, setChannel] = useState();
+
+    return <ChannelList onSelect={(channel) => router.push(`/channel/${channel.cid}`)} />
 }
--- src/app/(home)/_layout.tsx
+++ src/app/(home)/_layout.tsx
@@ -1,6 +1,7 @@
 import { Slot, Stack } from "expo-router";
 import { useEffect } from "react";
 import { StreamChat } from 'stream-chat';
+import { Chat, OverlayProvider } from 'stream-chat-expo';
 
 const client = StreamChat.getInstance('wfxy4f9quwxh');
 
@@ -13,10 +14,25 @@ export default function HomeLayout() {
               name: 'Jim Lahey',
               image: 'https://i.imgur.com/fR9Jz14.png',
             },
-            'user_token',
+            client.devToken('jlahey'),
           );
+
+          // const channel = client.channel('messaging', 'the_park', {
+          //   name: 'The Park',
+          // });
+          // await channel.watch();
       }
+
+      connect();
     }, [])
     
-    return <Slot />
+    return (
+      <OverlayProvider>
+        <Chat client={client}>
+          <Stack>
+            <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+          </Stack>
+        </Chat>
+      </OverlayProvider>
+    )
 }
