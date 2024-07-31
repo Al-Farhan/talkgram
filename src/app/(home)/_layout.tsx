75242874d05baf675bdcbaf22ed543b290dcb1aa
Farhan Shaikh <programmingwithfarhan@gmail.com>
2024-07-31 12:24:54 +0530

    Stream Chat Provider

--- src/app/(home)/_layout.tsx
+++ src/app/(home)/_layout.tsx
@@ -1,38 +1,15 @@
-import { Slot, Stack } from "expo-router";
-import { useEffect } from "react";
-import { StreamChat } from 'stream-chat';
-import { Chat, OverlayProvider } from 'stream-chat-expo';
-
-const client = StreamChat.getInstance('wfxy4f9quwxh');
+import ChatProvider from "@/src/providers/ChatProvider";
+import { Stack } from "expo-router";
+import { StreamChat } from "stream-chat";
 
 export default function HomeLayout() {
-    useEffect(() => {
-      const connect = async () => {
-        await client.connectUser(
-            {
-              id: 'jlahey',
-              name: 'Jim Lahey',
-              image: 'https://i.imgur.com/fR9Jz14.png',
-            },
-            client.devToken('jlahey'),
-          );
 
-          // const channel = client.channel('messaging', 'the_park', {
-          //   name: 'The Park',
-          // });
-          // await channel.watch();
-      }
-
-      connect();
-    }, [])
+  return (
     
-    return (
-      <OverlayProvider>
-        <Chat client={client}>
+        <ChatProvider>
           <Stack>
             <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
           </Stack>
-        </Chat>
-      </OverlayProvider>
-    )
+        </ChatProvider>
+  );
 }
