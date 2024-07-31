359dfc8acf5a76e432e40bf6b46c10ddec8cd28a
Farhan Shaikh <programmingwithfarhan@gmail.com>
2024-07-31 12:07:39 +0530

    Setup Streamchat UI

--- src/app/(home)/(tabs)/index.tsx
+++ src/app/(home)/(tabs)/index.tsx
@@ -1,9 +1,7 @@
 import { router } from "expo-router";
-import { useState } from "react";
 import { ChannelList} from "stream-chat-expo";
 
 export default function MainTabScreen() {
-    const [channel, setChannel] = useState();
 
     return <ChannelList onSelect={(channel) => router.push(`/channel/${channel.cid}`)} />
 }
