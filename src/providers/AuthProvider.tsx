1b9e601bf25ca975892573deceb5697c2674a44e
Farhan Shaikh <programmingwithfarhan@gmail.com>
2024-08-02 14:53:48 +0530

    Connect supabase user to streamchat user

--- src/providers/AuthProvider.tsx
+++ src/providers/AuthProvider.tsx
@@ -1,19 +1,28 @@
-import { createContext, PropsWithChildren, useContext, useEffect, useState } from "react";
+import {
+  createContext,
+  PropsWithChildren,
+  useContext,
+  useEffect,
+  useState,
+} from "react";
 import { Session, User } from "@supabase/supabase-js";
 import { supabase } from "../lib/supabase";
 
 type AuthContext = {
-    session: Session | null;
-    user: User | null
-}
+  session: Session | null;
+  user: User | null;
+  profile: any | null;
+};
 
 const AuthContext = createContext<AuthContext>({
-    session: null,
-    user: null,
+  session: null,
+  user: null,
+  profile: null
 });
 
 export default function AuthProvider({ children }: PropsWithChildren) {
   const [session, setSession] = useState<Session | null>(null);
+  const [profile, setProfile] = useState<any>();
 
   useEffect(() => {
     supabase.auth.getSession().then(({ data: { session } }) => {
@@ -24,8 +33,28 @@ export default function AuthProvider({ children }: PropsWithChildren) {
       setSession(session);
     });
   }, []);
+
+  useEffect(() => {
+    if (!session?.user) {
+      setProfile(null);
+      return;
+    }
+
+    const fetchProfile = async () => {
+      let { data, error } = await supabase
+        .from("profiles")
+        .select("*")
+        .eq('id', session.user.id)
+        .single();
+
+      setProfile(data);
+    };
+
+    fetchProfile();
+  }, [session?.user]);
+  
   return (
-    <AuthContext.Provider value={{ session, user: session?.user }}>
+    <AuthContext.Provider value={{ session, user: session?.user, profile }}>
       {children}
     </AuthContext.Provider>
   );
--- src/providers/ChatProvider.tsx
+++ src/providers/ChatProvider.tsx
@@ -2,20 +2,28 @@ import { PropsWithChildren, useEffect, useState } from "react";
 import { ActivityIndicator } from "react-native";
 import { StreamChat } from "stream-chat";
 import { Chat, OverlayProvider } from "stream-chat-expo";
+import { useAuth } from "./AuthProvider";
 
 const client = StreamChat.getInstance(String(process.env.EXPO_PUBLIC_STREAM_API_KEY));
 
 export default function ChatProvider({ children }: PropsWithChildren) {
     const [isReady, setIsReady] = useState(false);
+    const { profile } = useAuth();
+    
   useEffect(() => {
+    console.log("use Effect", profile);
+    
+    if (!profile) {
+      return;
+    }
     const connect = async () => {
       await client.connectUser(
         {
-          id: "jlahey",
-          name: "Jim Lahey",
+          id: profile.id,
+          name: profile.full_name,
           image: "https://i.imgur.com/fR9Jz14.png",
         },
-        client.devToken("jlahey")
+        client.devToken(profile.id)
       );
       setIsReady(true);
       // const channel = client.channel('messaging', 'the_park', {
@@ -27,10 +35,13 @@ export default function ChatProvider({ children }: PropsWithChildren) {
     connect();
 
     return () => {
-        client.disconnectUser();
-        setIsReady(false);
+        if (isReady) {
+          client.disconnectUser();
+          setIsReady(false);
+        }
+        
     }
-  }, []);
+  }, [profile?.id]);
 
   if (!isReady) {
     return <ActivityIndicator />;
